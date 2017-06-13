02a713902e772418ef8080c1c8f0214c0888f0b7
CJ <cj@cjbai.com>
2017-06-12 21:37:47 -0400

    Initial commit of tools

---
+++ Ping Loop.ps1
@@ -0,0 +1,24 @@
+﻿#initial val
+$x = [int]1
+$i = "192.168.1."
+$c = ""
+$h = [string]$x
+
+for ()
+{
+    $h = [string]$x
+    $c = $i + $h
+    echo $c
+    echo $x
+    echo " "
+    echo " "
+
+    ping -n 1 -w 15 $c
+
+    if ($x.Equals(22)) 
+    {
+    echo "end"
+    break;
+    }
+    $x = $x + 1
+}
